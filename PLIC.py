0724fc7c65d95f289dcd604e57d47f660282df4f
cheesy94 <97102775+cheesy94@users.noreply.github.com>
2022-12-29 15:59:45 +0100

    Update PLIC.py

--- PLIC.py
+++ PLIC.py
@@ -8,35 +8,6 @@ Created on Wed Jan 27 01:56:13 2021
 import numpy as np
 import matplotlib.pyplot as plt
 
-def PLIC_a(c,b,x,y):
-    vof = 0.5-abs(0.5-c)
-    dx = np.sqrt(2*abs(b[1]/b[0])*vof*np.diff(x)[0]*np.diff(y)[0])
-    dy = abs(b[0]/b[1])*dx
-    
-    if (c-0.5)*(b[0])<0:
-        xx = np.array([x[0], x[0]+dx])
-        yy = np.array([dy, 0])
-    else:
-        xx = np.array([x[1]-dx, x[1]])
-        yy = np.array([0, dy])
-        
-    if (c-0.5)*(b[1])<0:
-        yy += y[0]
-    else:
-        yy = y[1]-yy
-    
-    return (xx,yy)
-
-def PLIC_b(c,b,x,y):
-    vof = 0.5-b[1]*(0.5-c)/abs(b[1])
-    
-    return
-
-def PLIC_c(c,b,x,y):
-    vof = 0.5-b[0]*(0.5-c)/abs(b[0])
-    
-    return
-
 def PLIC(c,b,x,y):
     Dx = np.diff(x)[0]
     Dy = np.diff(y)[0]
@@ -89,7 +60,7 @@ ycoords = (2,3)
 
 xx,yy = PLIC(c,n,xcoords,ycoords)
 
-# TEST
+# TEST PLOT
 plt.figure()
 plt.plot(xx,yy)
 plt.xlim(xcoords)
