22e9c36b7f5ff374ba51f7ae224419161f68bc5f
cheesy94 <97102775+cheesy94@users.noreply.github.com>
2023-01-03 21:42:56 +0100

    Correct horizontal and vertical cases

--- PLIC.py
+++ PLIC.py
@@ -12,28 +12,36 @@ def get_interface_2D(c,b,x,y):
     Dy = np.diff(y)[0]
     
     vof = 0.5-abs(0.5-c)
-    dx = np.sqrt(2*abs(b[1]/b[0])*vof*Dx*Dy)
-    dy = -b[0]/b[1]*min(dx,Dx)
+    
+    if b[0]:
+        dx = np.sqrt(2*abs(b[1]/b[0])*vof*Dx*Dy)
+    else:
+        dx = Dx*2 # horizontal case
+    
+    if b[1]:
+        dy = -b[0]/b[1]*min(dx,Dx)
+    else:
+        dy = Dy*2 # vertical case
     
     if dx>Dx:
-        #caso b
-        xx = x
+        # almost horizontal case
+        xx = np.array(x)
         
         vof = 0.5-b[1]*(0.5-c)/abs(b[1])
         y2 = vof*Dy-Dx/2*b[0]/b[1]+y[0]
         yy = np.array([y2-dy,y2])
         
     elif abs(dy)>Dy:
-        #caso c
+        # almost vertical case
         dx = -b[1]/b[0]*Dy
-        yy = y
+        yy = np.array(y)
         
         vof = 0.5-b[0]*(0.5-c)/abs(b[0])
         x2 = vof*Dx-Dy/2*b[1]/b[0]+x[0]
         xx = np.array([x2-dx,x2])
         
     else:
-        #caso a
+        # small triangle case
         if (c-0.5)*(b[0])<0:
             xx = np.array([x[0],x[0]+dx])
             yy = np.array([-dy, 0])
