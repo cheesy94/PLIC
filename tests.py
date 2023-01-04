4ba063e4df3524e364a5aded283e6f1619be660d
cheesy94 <97102775+cheesy94@users.noreply.github.com>
2023-01-04 01:20:27 +0100

    Add normal vector calculation from vof field

--- tests.py
+++ tests.py
@@ -9,6 +9,7 @@ from PLIC import get_interface_2D
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 ### SIMPLE TEST
 
 # cell vof
@@ -29,27 +30,36 @@ plt.xlim(xcoords)
 plt.ylim(ycoords)
 
 
-
 ### IMAGE TEST
 gridsize = (3,3)
 
 # cells vof
-c = np.array([1,1,0.18,
-              1,0.86,0.015,
-              0.7,0.15,0])
+c = np.array([1,1,0.18,#0,
+              1,0.86,0.015,#0,
+              0.7,0.15,0])#,0,
+              #0,0,0,0])
 
-# normal vectors
-n = np.array([(),(),(0.1,0.9),
-               (),(0.4,0.6),(0.3,0.7),
-               (0.8,0.2),(0.5,0.5),()],dtype=object)
+# normal vectors assignment
+#n = np.array([(),(),(0.1,0.9),
+#               (),(0.4,0.6),(0.3,0.7),
+#               (0.8,0.2),(0.5,0.5),()],dtype=object)
+
+# normal vectors calculation from vof field
+nx,ny = np.gradient(-c.reshape(gridsize))
+nxr = nx.copy()
+#nxr[(0,-1),:] /= 2
+nxr = nxr.ravel()
+nyr = ny.copy()
+#nyr[:,(0,-1)] /= 2
+nyr = nyr.ravel()
 
 # cell sizes
-xc = np.linspace(0,3,3+1)
-yc = np.linspace(1,4,3+1)
+xc = np.linspace(0,0+gridsize[0],gridsize[0]+1)
+yc = np.linspace(1,1+gridsize[1],gridsize[1]+1)
 xcoords,ycoords = np.meshgrid(xc,yc,indexing='ij')
 
-xx = np.empty_like(n)
-yy = np.empty_like(n)
+xx = np.empty_like(c,dtype=object)
+yy = np.empty_like(c,dtype=object)
 
 plt.figure()
 
@@ -59,7 +69,7 @@ for e in range(xx.size):
         
     i = int(e/gridsize[1])
     j = e%gridsize[1]
-    xx[e],yy[e] = get_interface_2D(c[e],n[e],xc[i:i+2],yc[j:j+2])
+    xx[e],yy[e] = get_interface_2D(c[e],(nxr[e],nyr[e]),xc[i:i+2],yc[j:j+2])
     plt.plot(xx[e],yy[e],'k--')
     
 plt.xlim(xc.min(),xc.max())
